aa1ef85bc9fc00a0187568dac879d802c5dc0be5
Joao Rodrigues <jlrodrigues87@gmail.com>
2014-04-25 21:09:53 +0100

    close #3

--- exec_vagrant_ciserver.sh
+++ exec_vagrant_ciserver.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i hosts_vagrant --private-key=~/.vagrant.d/insecure_private_key --user=vagrant ciserver.yml
+ansible-playbook -i hosts_vagrant --private-key=~/.vagrant.d/insecure_private_key --user=vagrant ci_sites.yml
